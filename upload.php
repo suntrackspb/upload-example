0536c7b4f1409f03f669949b69e31d6bab3244ab
SunTrack <spbwar@gmail.com>
2020-09-30 21:59:44 +0300

    upload

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+// SETTINGS //
+$uploaddir = 'upload/'; // Relative Upload Location of data file
+$privacykey = 'ENTER KEY HERE'; // Key generate on Uploader Software
+// END //
+
+// CODE //
+$key = $_GET["Key"];
+if ( $key == $privacykey ){
+	if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+	$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+	echo "File ". $_FILES['file']['name'] ." uploaded successfully. ";
+
+	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+		//echo "File is valid, and was successfully moved. ";
+	}
+
+	else
+	print_r($_FILES);
+	}
+
+	else {
+	echo "Upload Failed!!!";
+	print_r($_FILES);
+	}
+} else {
+	echo "Not vertyfy";
+}
+// END //
+?>
